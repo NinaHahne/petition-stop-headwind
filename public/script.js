3ba0f19f0922f0c3e3ff8acd55992263aedfabc2
Nina Hahne <ninamariehahne@gmail.com>
2020-01-10 13:03:39 +0100

    w6 petition part1 - signature working but will connect all lines

--- public/script.js
+++ public/script.js
@@ -1,41 +1,59 @@
 // canvas stuff
 // console.log('yay canvas');
 
-const $canvas = $('#canvas');
+const $canvas = $("#canvas");
+const canvas = document.getElementById("canvas");
+const sigInput = document.getElementById("sig");
 
-const canvas = document.getElementById('canvas');
-const sigInput = document.getElementById('sig');
 let ctx;
+let mouseX, mouseY, mouseXstart, mouseYstart;
 
 if (canvas) {
-    ctx = canvas.getContext('2d');
+    ctx = canvas.getContext("2d");
     ctx.lineWidth = 2;
 
     ctx.strokeStyle = "midnightblue";
 
-    $canvas.on('mousedown', e => {
+    $canvas.on("mousedown", e => {
         startSignature(e);
+        $canvas.on("mousemove", e => {
+            drawLine(e);
+        });
     });
 
-    $canvas.on('mouseup', e => {
+    $canvas.on("mouseup", e => {
         saveSignature(e);
     });
 
 }
 
-function startSignature (e) {
-    console.log('signature started');
-    let mouseX = e.pageX - e.target.offsetLeft;
-    let mouseY = e.pageY - e.target.offsetTop;
-    console.log('mouseX: ', mouseX);
-    console.log('mouseY: ', mouseY);
-    ctx.beginPath();
-    ctx.arc(mouseX, mouseY, 20, 0, 2 * Math.PI);
-    ctx.stroke();
+function startSignature() {
+    console.log("signature started");
+    // mouseX = e.pageX - e.target.offsetLeft;
+    // mouseY = e.pageY - e.target.offsetTop;
+    // console.log("mouseX: ", mouseX);
+    // console.log("mouseY: ", mouseY);
+    // ctx.beginPath();
+    // ctx.arc(mouseX, mouseY, 20, 0, 2 * Math.PI);
+    // ctx.stroke();
 }
 
-function saveSignature (e) {
+function saveSignature() {
     // let dataURL = canvas.toDataURL();
     // console.log(dataURL);
     sigInput.value = canvas.toDataURL();
+    $canvas.unbind('mousemove');
+}
+
+function drawLine(e) {
+    mouseXstart = mouseX;
+    mouseYstart = mouseY;
+
+    mouseX = e.pageX - e.target.offsetLeft;
+    mouseY = e.pageY - e.target.offsetTop;
+
+    ctx.beginPath();
+    ctx.moveTo(mouseXstart, mouseYstart);
+    ctx.lineTo(mouseX, mouseY);
+    ctx.stroke();
 }
