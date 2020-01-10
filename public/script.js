f164f1e90159ddff839e1fd3aa335aba7f1947f4
Nina Hahne <ninamariehahne@gmail.com>
2020-01-10 13:56:36 +0100

    w6 tidied canvas code

--- public/script.js
+++ public/script.js
@@ -1,6 +1,3 @@
-// canvas stuff
-// console.log('yay canvas');
-
 const $canvas = $("#canvas");
 const canvas = document.getElementById("canvas");
 const sigInput = document.getElementById("sig");
@@ -11,7 +8,6 @@ let mouseX, mouseY, mouseXstart, mouseYstart;
 if (canvas) {
     ctx = canvas.getContext("2d");
     ctx.lineWidth = 2;
-
     ctx.strokeStyle = "midnightblue";
 
     $canvas.on("mouseenter", () => {
@@ -21,40 +17,32 @@ if (canvas) {
                 drawLine(e);
             });
         });
-
-        $canvas.on("mouseup", e => {
-            saveSignature(e);
-            $canvas.unbind('mousemove');
+        $canvas.on("mouseup", () => {
+            saveSignature();
+            $canvas.unbind("mousemove");
         });
-
         $canvas.on("mouseleave", () => {
-            $canvas.unbind('mousemove');
+            $canvas.unbind("mousemove");
         });
     });
-
 }
 
 function startSignature(e) {
-    console.log("signature started");
     mouseXstart = e.pageX - e.target.offsetLeft;
     mouseYstart = e.pageY - e.target.offsetTop;
 }
 
 function saveSignature() {
-    // let dataURL = canvas.toDataURL();
-    // console.log(dataURL);
     sigInput.value = canvas.toDataURL();
 }
 
 function drawLine(e) {
     mouseX = e.pageX - e.target.offsetLeft;
     mouseY = e.pageY - e.target.offsetTop;
-
     ctx.beginPath();
     ctx.moveTo(mouseXstart, mouseYstart);
     ctx.lineTo(mouseX, mouseY);
     ctx.stroke();
-
     mouseXstart = mouseX;
     mouseYstart = mouseY;
 }
